3fb8583e08291ece8b8d9af53d145fa1f931f6d9
Brian Humphreys <humbfrancis@gmail.com>
2021-11-08 07:58:50 -0600

    $1

--- alpaca_trade_api/rest.py
+++ alpaca_trade_api/rest.py
@@ -203,6 +203,8 @@ class REST(object):
 
     def data_get(self, path, data=None, api_version='v1'):
         base_url: URL = get_data_url()
+        print('base')
+        print(basse_url)
         return self._request(
             'GET', path, data, base_url=base_url, api_version=api_version,
         )
